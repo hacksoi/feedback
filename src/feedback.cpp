55c2179043c140155900092f75112c7a956377f2
hacksoi <sohackinp@gmail.com>
2017-07-30 22:50:47 -0700

    added basic IMGUI

--- src/feedback.cpp
+++ src/feedback.cpp
@@ -8,6 +8,8 @@
 #include "GL/glew.h"
 #include "GL/wglew.h"
 
+#include "imgui/imgui.h"
+
 #define Kilobytes(NumberOfKbs) (NumberOfKbs * 1024)
 #define Megabytes(NumberOfMbs) (NumberOfMbs * 1024 * 1024)
 #define Gigabytes(NumberOfGbs) (NumberOfGbs * 1024 * 1024 * 1024)
@@ -23,6 +25,11 @@
 
 static HDC GlobalDeviceContextHandle;
 
+struct color
+{
+    float R, G, B, A;
+};
+
 struct time_point
 {
     float Time, Y;
@@ -32,6 +39,8 @@ struct app_state
 {
     uint32_t WindowWidth;
     uint32_t WindowHeight;
+
+    color ClearColor;
 
     uint32_t ShaderProgram;
     uint32_t Vbo;
@@ -83,6 +92,13 @@ main()
 )STR";
 
 static char GeneralBuffer[512];
+
+/* IMGUI */
+static GLuint       g_FontTexture = 0;
+static int          g_ShaderHandle = 0, g_VertHandle = 0, g_FragHandle = 0;
+static int          g_AttribLocationTex = 0, g_AttribLocationProjMtx = 0;
+static int          g_AttribLocationPosition = 0, g_AttribLocationUV = 0, g_AttribLocationColor = 0;
+static unsigned int g_VboHandle = 0, g_VaoHandle = 0, g_ElementsHandle = 0;
 
 static void
 ErrorExit(char *Message)
@@ -139,6 +155,21 @@ Win32GetElapsedTime(float StartTime)
 {
     float ElapsedTime = Win32GetCurrentTime() - StartTime;
     return ElapsedTime;
+}
+
+inline static void
+glClearColor(color Color)
+{
+    glClearColor(Color.R, Color.G, Color.B, Color.A);
+}
+
+static void
+SetColor(color *Color, float R, float G, float B, float A)
+{
+    Color->R = R;
+    Color->G = G;
+    Color->B = B;
+    Color->A = A;
 }
 
 typedef void check_function(GLuint object, GLenum name, GLint *params);
@@ -268,20 +299,228 @@ HandleTraceModeMovement(app_state *AppState, int FlippedY)
 }
 
 static void
-DrawTimePoints(app_state *AppState)
+RedrawScreen(app_state *AppState)
 {
     float CameraTimePosCentered = AppState->CameraTimePos - (AppState->WindowWidth / 2 / AppState->PixelsPerSecond);
     glUniform1f(GetUniformLocation(AppState->ShaderProgram, "CameraTimePos"), CameraTimePosCentered);
-
     glUniform1f(GetUniformLocation(AppState->ShaderProgram, "PixelsPerSecond"), AppState->PixelsPerSecond);
 
+    glClearColor(AppState->ClearColor);
     glClear(GL_COLOR_BUFFER_BIT);
 
     glUseProgram(AppState->ShaderProgram);
     glBindVertexArray(AppState->Vao);
     glDrawArrays(GL_LINE_STRIP, 0, AppState->TimePointsInVbo + 1);
 
+    ImGui::NewFrame();
+    ImGui::ShowTestWindow();
+    ImGui::Render();
+
     SwapBuffers(GlobalDeviceContextHandle);
+}
+
+static void
+ModifyPixelsPerSecond(app_state *AppState, float Delta)
+{
+    AppState->PixelsPerSecond += Delta;
+
+    if(AppState->PixelsPerSecond < 1)
+    {
+        AppState->PixelsPerSecond = 1;
+    }
+
+    Printf("%f\n", AppState->PixelsPerSecond);
+}
+
+static void
+ModifyCameraTimePos(app_state *AppState, float Delta)
+{
+    AppState->CameraTimePos += Delta;
+}
+
+void ImGui_CreateDeviceObjects()
+{
+    // Backup GL state
+    GLint last_texture, last_array_buffer, last_vertex_array;
+    glGetIntegerv(GL_TEXTURE_BINDING_2D, &last_texture);
+    glGetIntegerv(GL_ARRAY_BUFFER_BINDING, &last_array_buffer);
+    glGetIntegerv(GL_VERTEX_ARRAY_BINDING, &last_vertex_array);
+
+    const GLchar *vertex_shader =
+        "#version 330\n"
+        "uniform mat4 ProjMtx;\n"
+        "in vec2 Position;\n"
+        "in vec2 UV;\n"
+        "in vec4 Color;\n"
+        "out vec2 Frag_UV;\n"
+        "out vec4 Frag_Color;\n"
+        "void main()\n"
+        "{\n"
+        "	Frag_UV = UV;\n"
+        "	Frag_Color = Color;\n"
+        "	gl_Position = ProjMtx * vec4(Position.xy,0,1);\n"
+        "}\n";
+
+    const GLchar* fragment_shader =
+        "#version 330\n"
+        "uniform sampler2D Texture;\n"
+        "in vec2 Frag_UV;\n"
+        "in vec4 Frag_Color;\n"
+        "out vec4 Out_Color;\n"
+        "void main()\n"
+        "{\n"
+        "	Out_Color = Frag_Color * texture( Texture, Frag_UV.st);\n"
+        "}\n";
+
+    g_ShaderHandle = glCreateProgram();
+    g_VertHandle = glCreateShader(GL_VERTEX_SHADER);
+    g_FragHandle = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(g_VertHandle, 1, &vertex_shader, 0);
+    glShaderSource(g_FragHandle, 1, &fragment_shader, 0);
+    glCompileShader(g_VertHandle);
+    glCompileShader(g_FragHandle);
+    glAttachShader(g_ShaderHandle, g_VertHandle);
+    glAttachShader(g_ShaderHandle, g_FragHandle);
+    glLinkProgram(g_ShaderHandle);
+
+    g_AttribLocationTex = glGetUniformLocation(g_ShaderHandle, "Texture");
+    g_AttribLocationProjMtx = glGetUniformLocation(g_ShaderHandle, "ProjMtx");
+    g_AttribLocationPosition = glGetAttribLocation(g_ShaderHandle, "Position");
+    g_AttribLocationUV = glGetAttribLocation(g_ShaderHandle, "UV");
+    g_AttribLocationColor = glGetAttribLocation(g_ShaderHandle, "Color");
+
+    glGenBuffers(1, &g_VboHandle);
+    glGenBuffers(1, &g_ElementsHandle);
+
+    glGenVertexArrays(1, &g_VaoHandle);
+    glBindVertexArray(g_VaoHandle);
+    glBindBuffer(GL_ARRAY_BUFFER, g_VboHandle);
+    glEnableVertexAttribArray(g_AttribLocationPosition);
+    glEnableVertexAttribArray(g_AttribLocationUV);
+    glEnableVertexAttribArray(g_AttribLocationColor);
+
+#define OFFSETOF(TYPE, ELEMENT) ((size_t)&(((TYPE *)0)->ELEMENT))
+    glVertexAttribPointer(g_AttribLocationPosition, 2, GL_FLOAT, GL_FALSE, sizeof(ImDrawVert), (GLvoid*)OFFSETOF(ImDrawVert, pos));
+    glVertexAttribPointer(g_AttribLocationUV, 2, GL_FLOAT, GL_FALSE, sizeof(ImDrawVert), (GLvoid*)OFFSETOF(ImDrawVert, uv));
+    glVertexAttribPointer(g_AttribLocationColor, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(ImDrawVert), (GLvoid*)OFFSETOF(ImDrawVert, col));
+#undef OFFSETOF
+
+    // Load font
+    uint8_t *Pixels;
+    int Width, Height;
+    ImGui::GetIO().Fonts->GetTexDataAsRGBA32(&Pixels, &Width, &Height);
+
+    glGenTextures(1, &g_FontTexture);
+    glBindTexture(GL_TEXTURE_2D, g_FontTexture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, Width, Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, Pixels);
+
+    ImGui::GetIO().Fonts->TexID = (void *)(uint64_t)g_FontTexture;
+
+    // Restore modified GL state
+    glBindTexture(GL_TEXTURE_2D, last_texture);
+    glBindBuffer(GL_ARRAY_BUFFER, last_array_buffer);
+    glBindVertexArray(last_vertex_array);
+}
+
+static void
+ImGui_RenderFunction(ImDrawData* draw_data)
+{
+    // Avoid rendering when minimized, scale coordinates for retina displays (screen coordinates != framebuffer coordinates)
+    ImGuiIO& io = ImGui::GetIO();
+    int fb_width = (int)(io.DisplaySize.x * io.DisplayFramebufferScale.x);
+    int fb_height = (int)(io.DisplaySize.y * io.DisplayFramebufferScale.y);
+    if (fb_width == 0 || fb_height == 0)
+        return;
+    draw_data->ScaleClipRects(io.DisplayFramebufferScale);
+
+    // Backup GL state
+    GLint last_active_texture; glGetIntegerv(GL_ACTIVE_TEXTURE, &last_active_texture);
+    glActiveTexture(GL_TEXTURE0);
+    GLint last_program; glGetIntegerv(GL_CURRENT_PROGRAM, &last_program);
+    GLint last_texture; glGetIntegerv(GL_TEXTURE_BINDING_2D, &last_texture);
+    GLint last_array_buffer; glGetIntegerv(GL_ARRAY_BUFFER_BINDING, &last_array_buffer);
+    GLint last_element_array_buffer; glGetIntegerv(GL_ELEMENT_ARRAY_BUFFER_BINDING, &last_element_array_buffer);
+    GLint last_vertex_array; glGetIntegerv(GL_VERTEX_ARRAY_BINDING, &last_vertex_array);
+    GLint last_blend_src_rgb; glGetIntegerv(GL_BLEND_SRC_RGB, &last_blend_src_rgb);
+    GLint last_blend_dst_rgb; glGetIntegerv(GL_BLEND_DST_RGB, &last_blend_dst_rgb);
+    GLint last_blend_src_alpha; glGetIntegerv(GL_BLEND_SRC_ALPHA, &last_blend_src_alpha);
+    GLint last_blend_dst_alpha; glGetIntegerv(GL_BLEND_DST_ALPHA, &last_blend_dst_alpha);
+    GLint last_blend_equation_rgb; glGetIntegerv(GL_BLEND_EQUATION_RGB, &last_blend_equation_rgb);
+    GLint last_blend_equation_alpha; glGetIntegerv(GL_BLEND_EQUATION_ALPHA, &last_blend_equation_alpha);
+    GLint last_viewport[4]; glGetIntegerv(GL_VIEWPORT, last_viewport);
+    GLint last_scissor_box[4]; glGetIntegerv(GL_SCISSOR_BOX, last_scissor_box);
+    GLboolean last_enable_blend = glIsEnabled(GL_BLEND);
+    GLboolean last_enable_cull_face = glIsEnabled(GL_CULL_FACE);
+    GLboolean last_enable_depth_test = glIsEnabled(GL_DEPTH_TEST);
+    GLboolean last_enable_scissor_test = glIsEnabled(GL_SCISSOR_TEST);
+
+    // Setup render state: alpha-blending enabled, no face culling, no depth testing, scissor enabled
+    glEnable(GL_BLEND);
+    glBlendEquation(GL_FUNC_ADD);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glDisable(GL_CULL_FACE);
+    glDisable(GL_DEPTH_TEST);
+    glEnable(GL_SCISSOR_TEST);
+
+    // Setup viewport, orthographic projection matrix
+    glViewport(0, 0, (GLsizei)fb_width, (GLsizei)fb_height);
+    const float ortho_projection[4][4] =
+    {
+        { 2.0f/io.DisplaySize.x, 0.0f,                   0.0f, 0.0f },
+        { 0.0f,                  2.0f/-io.DisplaySize.y, 0.0f, 0.0f },
+        { 0.0f,                  0.0f,                  -1.0f, 0.0f },
+        {-1.0f,                  1.0f,                   0.0f, 1.0f },
+    };
+    glUseProgram(g_ShaderHandle);
+    glUniform1i(g_AttribLocationTex, 0);
+    glUniformMatrix4fv(g_AttribLocationProjMtx, 1, GL_FALSE, &ortho_projection[0][0]);
+    glBindVertexArray(g_VaoHandle);
+
+    for (int n = 0; n < draw_data->CmdListsCount; n++)
+    {
+        const ImDrawList* cmd_list = draw_data->CmdLists[n];
+        const ImDrawIdx* idx_buffer_offset = 0;
+
+        glBindBuffer(GL_ARRAY_BUFFER, g_VboHandle);
+        glBufferData(GL_ARRAY_BUFFER, (GLsizeiptr)cmd_list->VtxBuffer.Size * sizeof(ImDrawVert), (const GLvoid*)cmd_list->VtxBuffer.Data, GL_STREAM_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g_ElementsHandle);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, (GLsizeiptr)cmd_list->IdxBuffer.Size * sizeof(ImDrawIdx), (const GLvoid*)cmd_list->IdxBuffer.Data, GL_STREAM_DRAW);
+
+        for (int cmd_i = 0; cmd_i < cmd_list->CmdBuffer.Size; cmd_i++)
+        {
+            const ImDrawCmd* pcmd = &cmd_list->CmdBuffer[cmd_i];
+            if (pcmd->UserCallback)
+            {
+                pcmd->UserCallback(cmd_list, pcmd);
+            }
+            else
+            {
+                glBindTexture(GL_TEXTURE_2D, (GLuint)(intptr_t)pcmd->TextureId);
+                glScissor((int)pcmd->ClipRect.x, (int)(fb_height - pcmd->ClipRect.w), (int)(pcmd->ClipRect.z - pcmd->ClipRect.x), (int)(pcmd->ClipRect.w - pcmd->ClipRect.y));
+                glDrawElements(GL_TRIANGLES, (GLsizei)pcmd->ElemCount, sizeof(ImDrawIdx) == 2 ? GL_UNSIGNED_SHORT : GL_UNSIGNED_INT, idx_buffer_offset);
+            }
+            idx_buffer_offset += pcmd->ElemCount;
+        }
+    }
+
+    // Restore modified GL state
+    glUseProgram(last_program);
+    glBindTexture(GL_TEXTURE_2D, last_texture);
+    glActiveTexture(last_active_texture);
+    glBindVertexArray(last_vertex_array);
+    glBindBuffer(GL_ARRAY_BUFFER, last_array_buffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, last_element_array_buffer);
+    glBlendEquationSeparate(last_blend_equation_rgb, last_blend_equation_alpha);
+    glBlendFuncSeparate(last_blend_src_rgb, last_blend_dst_rgb, last_blend_src_alpha, last_blend_dst_alpha);
+    if (last_enable_blend) glEnable(GL_BLEND); else glDisable(GL_BLEND);
+    if (last_enable_cull_face) glEnable(GL_CULL_FACE); else glDisable(GL_CULL_FACE);
+    if (last_enable_depth_test) glEnable(GL_DEPTH_TEST); else glDisable(GL_DEPTH_TEST);
+    if (last_enable_scissor_test) glEnable(GL_SCISSOR_TEST); else glDisable(GL_SCISSOR_TEST);
+    glViewport(last_viewport[0], last_viewport[1], (GLsizei)last_viewport[2], (GLsizei)last_viewport[3]);
+    glScissor(last_scissor_box[0], last_scissor_box[1], (GLsizei)last_scissor_box[2], (GLsizei)last_scissor_box[3]);
 }
 
 int CALLBACK
@@ -466,12 +705,12 @@ WinMain(HINSTANCE hInstance,
     app_state AppState = {};
     AppState.WindowWidth = WindowWidth;
     AppState.WindowHeight = WindowHeight;
-
     AppState.TimePointsCapacity = Megabytes(1) / sizeof(time_point);
     AppState.TimePoints = (time_point *)malloc(AppState.TimePointsCapacity * sizeof(time_point));
-
     AppState.PixelsPerSecond = (float)WindowWidth / 5.0f;
-    float PixelsPerSecondDelta = 5.0f;
+    SetColor(&AppState.ClearColor, 0.2f, 0.3f, 0.3f, 1.0f);
+
+    float BasePixelsPerSecondDelta = 5.0f;
 
     glViewport(0, 0, WindowWidth, WindowHeight);
 
@@ -501,16 +740,12 @@ WinMain(HINSTANCE hInstance,
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void *)0);
     glEnableVertexAttribArray(0);
 
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    SwapBuffers(GlobalDeviceContextHandle);
-
     glLineWidth(2.0f);
 
     uint32_t RenderDelayMillis = 16;
     SetTimer(WindowHandle, SCROLL_TIMER_ID, RenderDelayMillis, NULL);
 
-    float SingleFrameTime = (float)RenderDelayMillis / 1000.0f;
+    AppState.SingleFrameTime = (float)RenderDelayMillis / 1000.0f;
 
     bool IsTraceMode = false;
     float TimeWhenExitedTraceMode = 0;
@@ -519,10 +754,30 @@ WinMain(HINSTANCE hInstance,
 
     AppState.StartTime = Win32GetCurrentTime();
 
+    /* IMGUI initialization. */
+
+    ImGui_CreateDeviceObjects();
+
+    // application init
+    ImGuiIO& ImGuiIO = ImGui::GetIO();
+    ImGuiIO.DisplaySize.x = (float)WindowWidth;
+    ImGuiIO.DisplaySize.y = (float)WindowHeight;
+    ImGuiIO.RenderDrawListsFn = ImGui_RenderFunction;
+
     MSG Message;
     while(GetMessage(&Message, NULL, 0, 0))
     {
         TranslateMessage(&Message);
+
+        switch(Message.message)
+        {
+            case WM_MOUSEMOVE:
+            case WM_LBUTTONUP:
+            case WM_LBUTTONDOWN:
+            {
+                ImGuiIO.MousePos = ImVec2((float)GET_X_LPARAM(Message.lParam), (float)GET_Y_LPARAM(Message.lParam));
+            } break;
+        }
 
         switch(Message.message)
         {
@@ -532,10 +787,9 @@ WinMain(HINSTANCE hInstance,
 
                 IsTraceMode = true;
 
-                if(IsTraceMode)
-                {
-                    HandleTraceModeMovement(&AppState, GET_Y_LPARAM(Message.lParam));
-                }
+                HandleTraceModeMovement(&AppState, GET_Y_LPARAM(Message.lParam));
+
+                ImGuiIO.MouseDown[0] = true;
             } break;
 
             case WM_LBUTTONUP:
@@ -543,29 +797,25 @@ WinMain(HINSTANCE hInstance,
                 TimeWhenExitedTraceMode = Win32GetElapsedTime(AppState.StartTime);
 
                 IsTraceMode = false;
+
+                ImGuiIO.MouseDown[0] = false;
             } break;
 
             case WM_MOUSEWHEEL:
             {
                 int WheelDelta = GET_WHEEL_DELTA_WPARAM(Message.wParam);
 
+                float PixelsPerSecondDelta = 0;
                 if(WheelDelta > 0)
                 {
-                    AppState.PixelsPerSecond += PixelsPerSecondDelta;
+                    PixelsPerSecondDelta = BasePixelsPerSecondDelta;
                 }
                 else if(WheelDelta < 0)
                 {
-                    AppState.PixelsPerSecond -= PixelsPerSecondDelta;
+                    PixelsPerSecondDelta = -BasePixelsPerSecondDelta;
                 }
 
-                if(AppState.PixelsPerSecond < 1)
-                {
-                    AppState.PixelsPerSecond = 1;
-                }
-
-                Printf("%f\n", AppState.PixelsPerSecond);
-
-                DrawTimePoints(&AppState);
+                ModifyPixelsPerSecond(&AppState, PixelsPerSecondDelta);
             } break;
 
             case WM_MOUSEMOVE:
@@ -580,16 +830,44 @@ WinMain(HINSTANCE hInstance,
                 {
                     if(Message.wParam & MK_RBUTTON)
                     {
-                        // move the camera position
-
                         int MouseDeltaX = MousePosX - LastMousePosX;
-                        AppState.CameraTimePos -= ((float)MouseDeltaX / 100.0f);
+                        float CameraTimePosDelta = -((float)MouseDeltaX / 100.0f);
 
-                        DrawTimePoints(&AppState);
+                        ModifyCameraTimePos(&AppState, CameraTimePosDelta);
                     }
                 }
 
                 LastMousePosX = MousePosX;
+            } break;
+
+            case WM_KEYDOWN:
+            {
+                switch(Message.wParam)
+                {
+                    case VK_UP:
+                    {
+                        ModifyPixelsPerSecond(&AppState, 1);
+                    } break;
+
+                    case VK_DOWN:
+                    {
+                        ModifyPixelsPerSecond(&AppState, -1);
+                    } break;
+
+                    case VK_LEFT:
+                    {
+                        ModifyCameraTimePos(&AppState, -1);
+                    } break;
+
+                    case VK_RIGHT:
+                    {
+                        ModifyCameraTimePos(&AppState, 1);
+                    } break;
+                }
+            } break;
+
+            case WM_KEYUP:
+            {
             } break;
 
             case WM_TIMER:
@@ -617,9 +895,9 @@ WinMain(HINSTANCE hInstance,
                     AppState.NumFramesPassedSinceLastMouseMove++;
 
                     AppState.CameraTimePos = Win32GetElapsedTime(AppState.StartTime);
-
-                    DrawTimePoints(&AppState);
                 }
+
+                RedrawScreen(&AppState);
             } break;
 
             default:
